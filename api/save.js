97dd72597720475abd3147121b594589d382952c
HEKENYT <78589891+HEKENYT@users.noreply.github.com>
2025-11-22 20:55:07 +0100

    Update save.js

--- api/save.js
+++ api/save.js
@@ -3,7 +3,7 @@ import admin from "firebase-admin";
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      type: "service_account",
+      type: process.env.FIREBASE_TYPE,
       project_id: process.env.FIREBASE_PROJECT_ID,
       private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
       private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
